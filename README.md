192cf736ed206d63cb5ee7879a90fcafc53d44c0
Krishna Kumar <krishnak@utexas.edu>
2021-11-12 14:24:35 -0600

    :computer: Create an MLP

---
+++ README.md
@@ -0,0 +1,1 @@
+# Graph Network Simulator
---
+++ gns.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import torch
+import torch.nn as nn
+
+
+def build_mlp(
+        input_size: int,
+        hidden_layer_sizes: List[int],
+        output_size: int = None,
+        output_activation: torch.nn.Module = torch.nn.Identity,
+        activation: torch.nn.Module = torch.nn.ReLU) -> torch.nn.Module:
+    """Build a MultiLayer Perceptron.
+
+    Args:
+      input_size: Size of input layer.
+      layer_sizes: An array of input size for each hidden layer.
+      output_size: Size of the output layer.
+      output_activation: Activation function for the output layer.
+      activation: Activation function for the hidden layers.
+
+    Returns:
+      torch.nn.Sequential: A sequantial container.
+    """
+    # Size of each layer
+    layer_sizes = [input_size] + hidden_layer_sizes
+    if output_size:
+        layer_sizes.append(output_size)
+
+    # Number of layers
+    nlayers = len(layer_sizes) - 1
+
+    # Create a list of activation functions and
+    # set the last element to output activation function
+    acts = [activation for i in range(nlayers)]
+    acts[-1] = output_activation
+
+    # Create a torch sequential container
+    layers = []
+    for i in range(nlayers):
+        layers.append([torch.nn.Linear(layer_sizes[i],
+                                       layer_sizes[i + 1]), acts[i]()])
+    return torch.nn.Sequential(*layers)
