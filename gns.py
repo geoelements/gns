6044a1a2e4cc88c86606ed5270e78841cdaa8e25
Krishna Kumar <krishnak@utexas.edu>
2021-11-17 09:06:01 -0600

    :computer: InteractionNetwork derived from Torch Geometric MessagePassing

--- gns.py
+++ gns.py
@@ -1,6 +1,7 @@
 from typing import List
 import torch
 import torch.nn as nn
+from torch_geometric.nn import MessagePassing
 
 
 def build_mlp(
@@ -84,8 +85,100 @@ class Encoder(nn.Module):
         Args:
           x: Particle state representation as a torch tensor with shape (nparticles, nnode_input_features)
           edge_features: Edge features as a torch tensor with shape (nparticles, nedge_input_features)
+
         """
         return self.node_fn(x), self.edge_fn(edge_features)
+
+
+class InteractionNetwork(MessagePassing):
+    def __init__(
+        self,
+        nnode_in: int,
+        nnode_out: int,
+        nedge_in: int,
+        nedge_out: int,
+        nmlp_layers: int,
+        mlp_hidden_dim: int,
+    ):
+        """InteractionNetwork derived from torch_geometric MessagePassing class
+
+        Args:
+          nnode_in: Number of node inputs (latent dimension of size 128).
+          nnode_out: Number of node outputs (latent dimension of size 128).
+          nedge_in: Number of edge inputs (latent dimension of size 128).
+          nedge_out: Number of edge output features (latent dimension of size 128).
+          nmlp_layer: Number of hidden layers in the MLP (typically of size 2).
+          mlp_hidden_dim: Size of the hidden layer (latent dimension of size 128).
+
+        """
+        # Aggregate features from neighbors
+        super(InteractionNetwork, self).__init__(aggr='add')
+        # Node MLP
+        self.node_fn = nn.Sequential(*[build_mlp(nnode_in + nedge_out, [mlp_hidden_dim for _ in range(nmlp_layers)], nnode_out),
+                                       nn.LayerNorm(nnode_out)])
+        # Edge MLP
+        self.edge_fn = nn.Sequential(*[build_mlp(nnode_in + nnode_in + nedge_in, [mlp_hidden_dim for _ in range(nmlp_layers)], nedge_out),
+                                       nn.LayerNorm(nedge_out)])
+
+    def forward(self,
+                x: torch.tensor,
+                edge_index: torch.tensor,
+                edge_features: torch.tensor):
+        """The forward hook runs when the InteractionNetwork class is instantiated
+
+        Args:
+          x: Particle state representation as a torch tensor with shape (nparticles, nnode_input_features)
+          edge_index: A torch tensor list of source and target nodes with shape (2, nedges)
+          edge_features: Edge features as a torch tensor with shape (nedges, nedge_in=latent_dim of 128)
+
+        Returns:
+          tuple: Updated node and edge features
+        """
+        # Save current particle state and edge features
+        x_current = x
+        edge_features_current = edge_features
+        # Start propagating messages.
+        # Takes in the edge indices and all additional data which is needed to construct messages and to update node embeddings.
+        x, edge_features = self.propagate(
+            edge_index=edge_index, x=x, edge_features=edge_features)
+
+        return x_current + x, edge_features_current + edge_features
+
+    def message(self,
+                x_i: torch.tensor,
+                x_j: torch.tensor,
+                e_features: torch.tensor) -> torch.tensor:
+        """Constructs message from j to i of edge $e_{i, j}$. Tensors :obj:`x` passed to :meth:`propagate` can be mapped to the
+        respective nodes :math:`i` and :math:`j` by appending :obj:`_i` or :obj:`_j` to the variable name, i.e., :obj:`x_i` and :obj:`x_j`.
+
+        Args:
+          x_i: Particle state representation as a torch tensor with shape (nparticles, nnode_in=latent_dim of 128) at node i
+          x_j: Particle state representation as a torch tensor with shape (nparticles, nnode_in=latent_dim of 128) at node j
+          edge_features: Edge features as a torch tensor with shape (nedges, nedge_in=latent_dim of 128)
+
+        """
+        # Concat edge features with a final shape of [nedges, latent_dim*3]
+        e_features = torch.cat([x_i, x_j, e_features], dim=-1)
+        e_features = self.edge_fn(e_features)
+        return e_features
+
+    def update(self,
+               x_updated: torch.tensor,
+               x: torch.tensor,
+               e_features: torch.tensor):
+        """Update the particle state representation
+
+        Args:
+          x: Particle state representation as a torch tensor with shape (nparticles, nnode_in=latent_dim of 128)
+          x_updated: Updated particle state representation as a torch tensor with shape (nparticles, nnode_in=latent_dim of 128)
+          edge_features: Edge features as a torch tensor with shape (nedges, nedge_out=latent_dim of 128)
+
+        Returns:
+          tuple: Updated node and edge features
+        """
+        x_updated = torch.cat([x_updated, x], dim=-1)
+        x_updated = self.node_fn(x_updated)
+        return x_updated, e_features
 
 
 class Decoder(nn.Module):
