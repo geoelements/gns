c6d20a3fbcfa571088355a6e4730cf99fc3ec43b
Krishna Kumar <krishnak@utexas.edu>
2021-11-17 10:49:53 -0600

    :computer: Process component of GNS

--- gns.py
+++ gns.py
@@ -134,15 +134,15 @@ class InteractionNetwork(MessagePassing):
         Returns:
           tuple: Updated node and edge features
         """
-        # Save current particle state and edge features
-        x_current = x
-        edge_features_current = edge_features
+        # Save particle state and edge features
+        x_residual = x
+        edge_features_residual = edge_features
         # Start propagating messages.
         # Takes in the edge indices and all additional data which is needed to construct messages and to update node embeddings.
         x, edge_features = self.propagate(
             edge_index=edge_index, x=x, edge_features=edge_features)
 
-        return x_current + x, edge_features_current + edge_features
+        return x + x_residual, edge_features + edge_features_residual
 
     def message(self,
                 x_i: torch.tensor,
@@ -182,6 +182,69 @@ class InteractionNetwork(MessagePassing):
         return x_updated, edge_features
 
 
+class Processor(MessagePassing):
+    """The Processor: $\mathcal{G} \rightarrow \mathcal{G}$ computes interactions among nodes
+    via $M$ steps of learned message-passing, to generate a sequence of updated latent
+    graphs, $G = (G_1 , ..., G_M ), where $G^{m+1| = GN^{m+1} (G^m )$.
+    It returns the final graph, $G^M = PROCESSOR(G^0)$. Message-passing allows information to
+    propagate and constraints to be respected: the number of message-passing steps required will
+    likely scale with the complexity of the interactions."""
+
+    def __init__(
+        self,
+        nnode_in: int,
+        nnode_out: int,
+        nedge_in: int,
+        nedge_out: int,
+        nmessage_passing_steps: int,
+        nmlp_layers: int,
+        mlp_hidden_dim: int,
+    ):
+        """Processor derived from torch_geometric MessagePassing class. The processor uses a stack
+        of $M$ $GNs$ (where $M$ is a hyperparameter) with identical structure, MLPs as internal edge
+        and node update functions, and either shared or unshared parameters. We use GNs without global features or
+        global updates (i.e., an interaction network) , and with a residual connections between the
+        input and output latent node and edge attributes.
+
+        Args:
+          nnode_in: Number of node inputs (latent dimension of size 128).
+          nnode_out: Number of node outputs (latent dimension of size 128).
+          nedge_in: Number of edge inputs (latent dimension of size 128).
+          nedge_out: Number of edge output features (latent dimension of size 128).
+          nmessage_passing_steps: Number of message passing steps.
+          nmlp_layer: Number of hidden layers in the MLP (typically of size 2).
+          mlp_hidden_dim: Size of the hidden layer (latent dimension of size 128).
+
+        """
+        super(Processor, self).__init__(aggr='max')
+        # Create a stack of M Graph Networks GNs.
+        self.gnn_stacks = nn.ModuleList([
+            InteractionNetwork(
+                nnode_in=nnode_in,
+                nnode_out=nnode_out,
+                nedge_in=nedge_in,
+                nedge_out=nedge_out,
+                nmlp_layers=nmlp_layers,
+                mlp_hidden_dim=mlp_hidden_dim,
+            ) for _ in range(nmessage_passing_steps)])
+
+    def forward(self,
+                x: torch.tensor,
+                edge_index: torch.tensor,
+                edge_features: torch.tensor):
+        """The forward hook runs when the Processor class is instantiated. Run through GNN stacks
+
+          Args:
+            x: Particle state representation as a torch tensor with shape (nparticles, latent_dim)
+            edge_index: A torch tensor list of source and target nodes with shape (2, nedges)
+            edge_features: Edge features as a torch tensor with shape (nparticles, latent_dim)
+
+        """
+        for gnn in self.gnn_stacks:
+            x, edge_features = gnn(x, edge_index, edge_features)
+        return x, edge_features
+
+
 class Decoder(nn.Module):
     """The Decoder: $\mathcal{G} \rightarrow \mathcal{Y} extracts the dynamics information from the nodes of the final latent
         graph, $y_i = \delta v (v_i^M)$"""
@@ -205,7 +268,8 @@ class Decoder(nn.Module):
         self.node_fn = build_mlp(
             nnode_in, [mlp_hidden_dim for _ in range(nmlp_layers)], nnode_out)
 
-    def forward(self, x):
+    def forward(self,
+                x: torch.tensor):
         """The forward hook runs when the Decoder class is instantiated
 
         Args:
