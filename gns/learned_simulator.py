1ac4558e2ca9899c59b78370a897fb4e4586b729
Krishna Kumar <krishnak@utexas.edu>
2021-11-18 17:08:20 -0600

    :computer: Generate graph edges

--- gns/learned_simulator.py
+++ gns/learned_simulator.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
+import numpy as np
 from gns import graph_network
-from typing import List, Dict
+from torch_geometric.nn import radius_graph
+from typing import Dict
 
 
 class LearnedSimulator(nn.Module):
@@ -16,31 +19,32 @@ class LearnedSimulator(nn.Module):
           nmlp_layers: int,
           mlp_hidden_dim: int,
           connectivity_radius: float,
-          boundaries: List,
+          boundaries: np.ndarray,
           normalization_stats: Dict,
           nparticle_types: int,
           particle_type_embedding_size,
           device="cpu"):
     """Initializes the model.
 
-      Args:
-        particle_dimensions: Dimensionality of the problem.
-        nnode_in: Number of node inputs.
-        nedge_in: Number of edge inputs.
-        latent_dim: Size of latent dimension (128)
-        nmessage_passing_steps: Number of message passing steps.
-        nmlp_layers: Number of hidden layers in the MLP (typically of size 2).
-        connectivity_radius: Scalar with the radius of connectivity.
-        boundaries: List of 2-tuples, containing the lower and upper boundaries 
-          of the cuboid containing the particles along each dimensions, matching
-          the dimensionality of the problem.
-        normalization_stats: Dictionary with statistics with keys "acceleration"
-          and "velocity", containing a named tuple for each with mean and std
-          fields, matching the dimensionality of the problem.
-        nparticle_types: Number of different particle types.
-        particle_type_embedding_size: Embedding size for the particle type.
-        device: Runtime device (cuda or cpu).
-      """
+    Args:
+      particle_dimensions: Dimensionality of the problem.
+      nnode_in: Number of node inputs.
+      nedge_in: Number of edge inputs.
+      latent_dim: Size of latent dimension (128)
+      nmessage_passing_steps: Number of message passing steps.
+      nmlp_layers: Number of hidden layers in the MLP (typically of size 2).
+      connectivity_radius: Scalar with the radius of connectivity.
+      boundaries: Array of 2-tuples, containing the lower and upper boundaries
+        of the cuboid containing the particles along each dimensions, matching
+        the dimensionality of the problem.
+      normalization_stats: Dictionary with statistics with keys "acceleration"
+        and "velocity", containing a named tuple for each with mean and std
+        fields, matching the dimensionality of the problem.
+      nparticle_types: Number of different particle types.
+      particle_type_embedding_size: Embedding size for the particle type.
+      device: Runtime device (cuda or cpu).
+
+    """
     super(LearnedSimulator, self).__init__()
     self._boundaries = boundaries
     self._connectivity_radius = connectivity_radius
@@ -62,3 +66,33 @@ class LearnedSimulator(nn.Module):
         mlp_hidden_dim=mlp_hidden_dim)
 
     self._device = device
+
+  def forward(self):
+    """Forward hook runs on class instantiation"""
+    pass
+
+  def _generate_graph_edges(
+          self,
+          node_features: torch.tensor,
+          nparticles_per_example: torch.tensor,
+          radius: float,
+          add_self_edges: bool = True):
+    """Generate graph edges to all points within a threshold radius
+
+    Args:
+      node_features: Node features with shape (nparticles, dim).
+      nparticles_per_example: Number of particles per example. Default is 2 
+        examples per batch.
+      radius: Threshold to construct edges to all points within the radius.
+      add_self_edges: Boolean flag to include self edge (default: True)
+    """
+    # Specify examples id for particles
+    batch_ids = torch.cat([torch.LongTensor([i for _ in range(n)])
+                          for i, n in enumerate(nparticles_per_example)]).to(self._device)
+    # radius_graph accepts r < radius not r <= radius
+    # A torch tensor list of source and target nodes with shape (2, nedges)
+    edge_index = radius_graph(
+        node_features, r=radius, batch=batch_ids, loop=add_self_edges)
+    receivers = edge_index[0, :]
+    senders = edge_index[1, :]
+    return receivers, senders
