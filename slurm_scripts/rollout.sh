db89a17a8e488055777908dd19294e392f5b3c1a
Joseph Vantassel <jvantassel@utexas.edu>
2021-12-22 13:34:47 -0600

    :fire: remove email from slurm scripts

--- slurm_scripts/rollout.sh
+++ slurm_scripts/rollout.sh
@@ -9,7 +9,6 @@
 #SBATCH -t 15:00:00          # Run time (hh:mm:ss)
 #SBATCH --mail-type=all      # Send email at begin and end of job
 #SBATCH -A BCS20003          # Project/Allocation name (req'd if you have more than 1)
-#SBATCH --mail-user=jvantassel@tacc.utexas.edu
 
 # fail on error
 #set -e
