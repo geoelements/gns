474b982b73d86cac85a271451e4ec726cb895e4c
Joseph Vantassel <jvantassel@utexas.edu>
2022-03-07 12:39:01 -0600

    :books: Modify rollout to use mpm results

--- slurm_scripts/rollout.sh
+++ slurm_scripts/rollout.sh
@@ -18,7 +18,7 @@ cd ..
 source start_venv.sh
 
 # assume data is already downloaded and hardcode WaterDropSample
-data="Sand"
+data="mpm-1to1"
 python3 -m gns.train --mode="rollout" \
 --data_path="${SCRATCH}/gns_pytorch/${data}/dataset/" \
 --model_path="${SCRATCH}/gns_pytorch/${data}/models/" \
