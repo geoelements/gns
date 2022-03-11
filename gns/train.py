12265551295df153c5cef04e4c9d2a2bd1e2e3e8
Joseph Vantassel <jvantassel@utexas.edu>
2022-03-11 10:21:56 -0600

    :fire: Remove left over tf code

--- gns/train.py
+++ gns/train.py
@@ -47,96 +47,6 @@ INPUT_SEQUENCE_LENGTH = 6  # So we can calculate the last 5 velocities.
 NUM_PARTICLE_TYPES = 9
 KINEMATIC_PARTICLE_ID = 3
 
-
-def prepare_inputs(tensor_dict):
-  """Prepares a single stack of inputs by calculating inputs and targets.
-
-  Computes n_particles_per_example, which is a tensor that contains information
-  about how to partition the axis - i.e. which nodes belong to which graph.
-
-  Adds a batch axis to `n_particles_per_example` and `step_context` so they can
-  later be batched using `batch_concat`. This batch will be the same as if the
-  elements had been batched via stacking.
-
-  Note that all other tensors have a variable size particle axis,
-  and in this case they will simply be concatenated along that
-  axis.
-
-  Args:
-    tensor_dict: A dict of tensors containing positions, and step context (
-    if available).
-
-  Returns:
-    A tuple of input features and target positions.
-
-  """
-  # Position is encoded as [sequence_length, num_particles, dim] but the model
-  # expects [num_particles, sequence_length, dim].
-  pos = tensor_dict['position']
-  pos = tf.transpose(pos, perm=[1, 0, 2])
-
-  # The target position is the final step of the stack of positions.
-  target_position = pos[:, -1]
-
-  # Remove the target from the input.
-  tensor_dict['position'] = pos[:, :-1]
-
-  # Compute the number of particles per example.
-  nparticles = tf.shape(pos)[0]
-  # Add an extra dimension for stacking via concat.
-  tensor_dict['n_particles_per_example'] = nparticles[tf.newaxis]
-
-  if 'step_context' in tensor_dict:
-    # Take the input global context. We have a stack of global contexts,
-    # and we take the penultimate since the final is the target.
-    tensor_dict['step_context'] = tensor_dict['step_context'][-2]
-    # Add an extra dimension for stacking via concat.
-    tensor_dict['step_context'] = tensor_dict['step_context'][tf.newaxis]
-  return tensor_dict, target_position
-
-
-def prepare_rollout_inputs(context, features):
-  """Prepares an inputs trajectory for rollout."""
-  out_dict = {**context}
-  # Position is encoded as [sequence_length, nparticles, dim] but the model
-  # expects [nparticles, sequence_length, dim].
-  pos = tf.transpose(features['position'], [1, 0, 2])
-  # The target position is the final step of the stack of positions.
-  target_position = pos[:, -1]
-  # Remove the target from the input.
-  out_dict['position'] = pos[:, :-1]
-  # Compute the number of nodes
-  out_dict['n_particles_per_example'] = [tf.shape(pos)[0]]
-  if 'step_context' in features:
-    out_dict['step_context'] = features['step_context']
-  out_dict['is_trajectory'] = tf.constant([True], tf.bool)
-  return out_dict, target_position
-
-
-def batch_concat(dataset, batch_size):
-  """We implement batching as concatenating on the leading axis."""
-
-  # We create a dataset of datasets of length batch_size.
-  windowed_ds = dataset.window(batch_size)
-
-  # The plan is then to reduce every nested dataset by concatenating. We can
-  # do this using tf.data.Dataset.reduce. This requires an initial state, and
-  # then incrementally reduces by running through the dataset
-
-  # Get initial state. In this case this will be empty tensors of the
-  # correct shape.
-  initial_state = tree.map_structure(
-      lambda spec: tf.zeros(  # pylint: disable=g-long-lambda
-          shape=[0] + spec.shape.as_list()[1:], dtype=spec.dtype),
-      dataset.element_spec)
-
-  # We run through the nest and concatenate each entry with the previous state.
-  def reduce_window(initial_state, ds):
-    return ds.reduce(initial_state, lambda x, y: tf.concat([x, y], axis=0))
-
-  return windowed_ds.map(
-      lambda *x: tree.map_structure(reduce_window, initial_state, x))
-
 def rollout(
         simulator: learned_simulator.LearnedSimulator,
         position: torch.tensor,
@@ -214,7 +124,6 @@ def predict(
   
   simulator.to(device)
   simulator.eval()
-
 
   # Output path
   if not os.path.exists(FLAGS.output_path):
