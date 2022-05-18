1239218ca22d90f6afb2d61f9251b7efd446a771
Joseph Vantassel <jvantassel@utexas.edu>
2022-05-18 12:04:16 -0500

    :bug: Fix hardcoded dimensions to handle 3d case

--- gns/train.py
+++ gns/train.py
@@ -328,8 +328,8 @@ def _get_simulator(
 
   simulator = learned_simulator.LearnedSimulator(
       particle_dimensions=metadata['dim'],
-      nnode_in=30,
-      nedge_in=3,
+      nnode_in=37 if metadata['dim'] == 3 else 30,
+      nedge_in=metadata['dim'] + 1,
       latent_dim=128,
       nmessage_passing_steps=10,
       nmlp_layers=2,
--- slurm_scripts/train.sh
+++ slurm_scripts/train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH -J pyt_mpm_train         # Job name
-#SBATCH -o pyt_mpm_train.o%j     # Name of stdout output file
-#SBATCH -e pyt_mpm_train.e%j     # Name of stderr error file
+#SBATCH -J pyt_sand3d_train         # Job name
+#SBATCH -o pyt_sand3d_train.o%j     # Name of stdout output file
+#SBATCH -e pyt_sand3d_train.e%j     # Name of stderr error file
 #SBATCH -p gpu-a100              # Queue (partition) name
 #SBATCH -N 1                     # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1                 # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 48:00:00          # Run time (hh:mm:ss)
 #SBATCH --mail-type=all      # Send email at begin and end of job
+#SBATCH --mail-user=jvantassel@tacc.utexas.edu
 #SBATCH -A OTH21021          # Project/Allocation name (req'd if you have more than 1)
 
 # fail on error
@@ -22,7 +23,8 @@ data="Sand-3D"
 python3 -m gns.train --data_path="${SCRATCH}/gns_pytorch/${data}/dataset/" \
 --model_path="${SCRATCH}/gns_pytorch/${data}/models/" \
 --output_path="${SCRATCH}/gns_pytorch/${data}/rollouts/" \
+--nsave_steps=10000 \
 --cuda_device_number=0 \
---ntraining_steps=5000000
-#--model_file="latest" \
-#--train_state_file="latest"
+--ntraining_steps=5000000 \
+--model_file="latest" \
+--train_state_file="latest"
